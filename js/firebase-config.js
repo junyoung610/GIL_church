b6fddb2db50dc657ea53969e214e385665bfeab1
junyoung610 <junyoungkim610@gmail.com>
2026-05-10 01:46:58 +0900

    로그인문제수정

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,8 @@
+// js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js"; // 👈 이 줄을 반드시 추가하세요!
 
 const firebaseConfig = {
   apiKey: "AIzaSyAbz5cKl1_NUJhcrShvH5FKWMJGGQfhbWY",
@@ -15,4 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app); // 추가하여 내보내기
+export const storage = getStorage(app);
